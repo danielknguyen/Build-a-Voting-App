0fa17f0e8c169082fa5cf5e36d58e88b580b04d8
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-03-15 19:25:23 -0700

    added routes to register, poolls, login pages

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,23 @@
 var routes = function(app) {
 
   app.get('/', function(req, res) {
+
     res.render('index.html');
+  });
+
+  app.get('/polls', function(req, res) {
+
+    res.render('polls.html')
+  });
+
+  app.get('/register', function(req, res) {
+
+    res.render('register.html');
+  });
+
+  app.get('/login', function(req, res) {
+
+    res.render('login.html');
   });
 };
 
