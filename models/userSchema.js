f20a82893139ccf764d22d57bf74eab102b19d4a
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-03-24 12:45:35 -0700

    template for dashboard and settings

---
+++ models/userSchema.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose'),
+    // mongoose schema method
+    Schema = mongoose.Schema,
+    // mongoose objectId
+    ObjectId = Schema.ObjectId,
+    bcrypt = require('bcrypt-nodejs');
+
+var db = require('../libs/db.js');
+
+var userSchema = new Schema({
+  id: ObjectId,
+  fname: {
+    type: String,
+    trim: true
+  },
+  lname: {
+    type: String,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+    trim: true
+  },
+  updated_at: {
+    type: Date,
+    trim: true
+  }
+});
+
+// generate hash method
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// we need to create a model using the userSchema
+// User model will take in db connection
+var User = db.model('User', userSchema);
+
+module.exports = User;
