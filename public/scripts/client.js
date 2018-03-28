82eedb1175e8c3880e386a65939bb2ccfea127e5
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-03-27 20:49:20 -0700

    add input option, ajax request to poll api, chartjs, fb share button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
   // button to add input field in poll options
   $('.options-button').on('click', function() {
 
-    var html = '<input type="text" class="form-control" name="options[]" placeholder="" required>';
+    var html = '<input type="text" class="form-control" name="options[]" placeholder="" required> maxlength="50"';
     var last = $('.optionsDiv').children().last().val();
     console.log(last);
 
@@ -47,5 +47,96 @@ $(document).ready(function() {
     }
   });
 
+  // get poll id to request data in ajax
+  // var pollId = document.getElementById("poll_name");
+  var pollIdVal = $('#poll_name').attr('value');
+
+  // send ajax request if retrieved pollIdVal is not undefined
+  if (pollIdVal !== undefined) {
+
+    $.ajax({
+      url: "http://localhost:27017/chartjs/polls/api/data/" + pollIdVal,
+      type: "GET",
+      success: function(data) {
+        // console.log(data.poll_name);
+        // console.log(data.data);
+        var options = data.options;
+        var optionsData = data.data;
+        var data = [];
+
+        for (var i = 0; i < optionsData.length; i++) {
+          data.push(optionsData[i].totalVotes);
+        };
+        // console.log(data);
+
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+          type: 'doughnut',
+          data: {
+              labels: options,
+              datasets: [{
+                  label: '# of Votes',
+                  data: data,
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255,99,132,1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                        display: false
+                      },
+                      gridLines: {
+                        display:false,
+                        drawBorder: false
+                      }
+                  }]
+              }
+          }
+        });
+      },
+      error: function(data) {
+        console.log('error data: ' + data);
+      }
+    });
+  };
+
+  // share fb
+  $.ajaxSetup({ cache: true });
+  $.getScript('https://connect.facebook.net/en_US/sdk.js', function() {
+
+    FB.init({
+      appId: '161499961229671',
+      version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+    });
+
+    $('#shareBtn').on('click', function() {
+
+      FB.ui({
+        method: 'share',
+        href: 'https://developers.facebook.com/docs/',
+      }, function(response) {
+
+      });
+
+    });
+
+  });
 
 });
